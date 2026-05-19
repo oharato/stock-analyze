bbca7837775b5d34faea2bcfc95032465c618b9a
oharato <oharato@live.jp>
2026-05-19 22:26:50 +0900

    2秒間隔

--- apps/stock-analyze-collector/src/run-fetch-stock-prices.ts
+++ apps/stock-analyze-collector/src/run-fetch-stock-prices.ts
@@ -10,8 +10,8 @@ import { LoggerService } from './services/logger.service.js';
 
 async function main() {
   const logger = new LoggerService();
-  // 3 seconds delay
-  const yahooFinanceClient = new YahooFinanceClient({ minIntervalMs: 3000 }, logger);
+  // 2 seconds delay
+  const yahooFinanceClient = new YahooFinanceClient({ minIntervalMs: 2000 }, logger);
   const stockRepository = new StockRepository(logger);
   const priceRepository = new PriceRepository(logger);
   const cliArgsService = new CliArgsService();
